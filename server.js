4c267d1816a49162c3cb0155d61b5006c1415e36
Violet <violetshirokuma@gmail.com>
2020-03-21 12:15:49 -0700

    Add files via upload
    There's a lot that needs to be edited in these files, e.g. specific routes

---
+++ server.js
@@ -0,0 +1,44 @@
+var express = require("express");
+var mysql = require("mysql");
+
+var db = require("./models");
+var dbFill = require("./opponets");
+
+var app = express();
+var PORT = process.env.PORT || 3500;
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+// Routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+var syncOptions = { force: true };
+
+////??????
+if (process.env.NODE_ENV === "test") {
+  syncOptions.force = true;
+}
+
+
+
+
+// Starting the server, syncing our models ------------------------------------/
+db.sequelize.sync(syncOptions).then(function() {
+  app.listen(PORT, function() {
+    dbFill.populate(rps_db);
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+
+    
+  });
+});
+
+module.exports = app;
